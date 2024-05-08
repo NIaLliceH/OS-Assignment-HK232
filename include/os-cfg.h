ed7fd3a72f04b9cdbf89fecf778c883b9cd6e80f
NIaLliceH <nialliceh@gmail.com>
2024-05-08 16:53:59 +0700

    synch for sched

--- include/os-cfg.h
+++ include/os-cfg.h
@@ -7,14 +7,14 @@
 #define CPU_TLB 
 #define CPUTLB_FIXED_TLBSZ
 #define MM_PAGING
-// #define MM_FIXED_MEMSZ
-#define VMDBG 1
-#define MMDBG 1
+#define MM_FIXED_MEMSZ
+// #define VMDBG 1
+// #define MMDBG 1
 #define IODUMP 1
 #define PAGETBL_DUMP 1
 
-#define TLB_DUMP
+// #define TLB_DUMP
 
-#define SYNC
+#define SYNCH
 
 #endif
